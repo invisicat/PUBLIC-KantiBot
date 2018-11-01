618dcfdff08eceb23c4ff4c5c44617a7994bcc51
AndyIsCool5463 <www.andyiscool@outlook.com>
2018-11-01 12:05:22 -0700

    Barebone basics of Minigames

---
+++ commands/minigame.js
@@ -0,0 +1,19 @@
+const Discord = require("discord.js");
+const settings = require('../settings.json')
+const modules = require('../modules/minigames.js')
+exports.run = async (Bot, message, args) => {
+    let game = args[0];
+    if(!args[0]) return message.reply("Enter a game!");
+    if(game == 'ttt') {
+      if(!args[1]) return message.reply("Enter a user to play against or enter: **comp**")
+      let user = message.author;
+      let user1 = message.mentions.users.first();
+      console.log(modules.tic_tac_toe(user, user1, true))
+    }
+}
+exports.help = {
+  name: "minigames",
+  category: "Fun",
+  description: "Play games!",
+  alias: "None"
+}
---
+++ modules/minigames.js
@@ -0,0 +1,5 @@
+const Discord = require('discord.js');
+module.exports.tic_tac_toe = function(user1, user2, int) {
+//  if(int == false) return;
+  return `${user1} is playing with ${user2}`
+}
